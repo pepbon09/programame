acb6f1da09fb019510c2a4998c0cc7d4c4ba07e9
Pep Bonafont <bonafontpep@gmail.com>
2019-10-05 02:57:40 +0200

    Problema 362 Hecho!

--- p362.java
+++ p362.java
@@ -1,18 +1,29 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class p362 {
-	
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+public class P362Navidad {
+
+	public static void main(String[] args) {
 		
-		int ncasos = sc.nextInt();
+		Scanner scn = new Scanner(System.in);
+		
+		int casos = scn.nextInt();
 		int dia;
 		int mes;
 
-		for(int i=0; i<ncasos; i++){
+		for(int i=0; i<casos; i++){
 			
+			dia = scn.nextInt();
+			mes = scn.nextInt();
+			
+			if(dia==25 && mes == 12) {
+				System.out.println("SI");
+			}
+			else {
+				System.out.println("NO");
+			}
 			
 		}
 
 	}
+
 }
